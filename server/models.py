238e5b1e1326fe73dd3b48f92b37f5f581e50b09
Stuart Forrester <stforrester@gmail.com>
2025-01-23 14:51:33 -0600

    completes Models and Validations deliverables

--- server/models.py
+++ server/models.py
@@ -21,8 +21,11 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    # Relationship mapping the restaurant to the related restaurant_pizzas
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='restaurant', cascade='all, delete-orphan')
 
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.restaurant",)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -36,8 +39,11 @@ class Pizza(db.Model, SerializerMixin):
     ingredients = db.Column(db.String)
 
     # add relationship
+    # Relationship mapping the pizza to the related restaurant_pizzas
+    restaurant_pizzas = db.relationship('RestaurantPizza', back_populates='pizza', cascade='all, delete-orphan')
 
     # add serialization rules
+    serialize_rules = ("-restaurant_pizzas.pizza",)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -50,15 +56,25 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
+    # Foreign key to store the pizza id
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+    # Foreign key to store the restaurant id
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+
+    # Relationship mapping the restaurant_pizza to the related pizza
+    pizza = db.relationship('Pizza', back_populates='restaurant_pizzas')
+    # Relationship mapping the restaurant_pizza to the related restaurant
+    restaurant = db.relationship('Restaurant', back_populates='restaurant_pizzas')
 
     # add serialization rules
+    serialize_rules = ("-restaurant.restaurant_pizzas", "-pizza.restaurant_pizzas",)
 
     # add validation
-    @validates("price")
-    def validate_price(self, key, price_int):
-        if not price_int and 1 <= price_int <= 30:
-            raise ValueError("Price must be an integer value between 1 and 30")
-        return price_int
+    @validates('price')
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30):
+            raise ValueError('Price must be between 1 and 30')
+        return value
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
